9f17052c1b0e5b971467d3624fcb9412150c776a
HAKANCSK\Hakan <coskunhakan1995@gmail.com>
2018-01-26 14:27:58 +0300

    Yönetim area oluşturuldu

---
+++ TechnicalService/TechnicalService.WebMVC.UI/Areas/Management/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechnicalService.WebMVC.UI.Areas.Management.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        // GET: Management/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
